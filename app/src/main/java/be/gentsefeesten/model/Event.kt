a1ce5de268597f16fad4e746208d8ec24736da41
Davy Gevaert <davy.gevaert@student.vives.be>
2022-06-18 17:46:55 +0200

    added Event model

---
+++ app/src/main/java/be/gentsefeesten/model/Event.kt
@@ -0,0 +1,39 @@
+package be.gentsefeesten.model
+
+import android.util.Log
+import java.io.Serializable
+
+class Event : Serializable {
+    var offer_availableatorfrom : String = ""
+    var superevent : String = ""
+    var keywords : String = ""
+    var location : String = ""
+    var offers_new : String = ""
+    var url : String = ""
+    var id : String = ""
+    var type : String = ""
+    var image : String = ""
+    var isaccessibleforfree : String = ""
+    var offer_pricecurrency : String = ""
+    var organizer : String = ""
+    var name : String = ""
+    var image_thumbnail : String = ""
+    var offer_description : String = ""
+    var contactpoint : String = ""
+    var created : String = ""
+    var theme : String = ""
+    var image_caption : String = ""
+    var description_nl : String = ""
+    var uuid : String = ""
+    var enddate : String = ""
+    var description : String = ""
+    var address : String = ""
+    var ispartof : String = ""
+    var name_nl : String = ""
+    var outdoors : String = ""
+    var image_path : String = ""
+    var offers : String = ""
+    var startdate : String = ""
+    var changed : String = ""
+    var iswheelchairunfriendly : String = ""
+}
